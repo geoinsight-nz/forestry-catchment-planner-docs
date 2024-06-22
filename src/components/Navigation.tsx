fae1e7b0439b7424e81d6070590a74ec17fe6fe9
Henry Babbage <hbabbage@gmail.com>
2024-06-22 17:20:08 +0200

    tidy up forest-stand-age and regions

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -237,7 +237,7 @@ export const navigation: Array<NavGroup> = [
       { title: 'Background', href: '/background' },
       { title: 'Regions', href: '/regions' },
       { title: 'Plantation forests', href: '/plantation-forests' },
-      { title: 'Stand age classification', href: '/stand-age-classification' },
+      { title: 'Forest stand age', href: '/forest-stand-age' },
       { title: 'Depositional zones', href: '/depositional-zones' },
       { title: 'Glossary', href: '/glossary' },
       { title: 'References', href: '/references' },
@@ -246,7 +246,7 @@ export const navigation: Array<NavGroup> = [
   },
   {
     title: 'Methodology',
-      links: [
+    links: [
       { title: 'Age class process', href: '/age-class-process' },
       { title: 'Clearfelling', href: '/clearfelling' },
       { title: 'Window of vulnerability', href: '/wov' },
