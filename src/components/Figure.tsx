fc50c4e08e941c011c3ddb0e7446b3ab94159e8a
Henry Babbage <hbabbage@gmail.com>
2024-06-22 18:33:55 +0200

    fix import

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -1,10 +1,12 @@
 import Image, { ImageProps } from 'next/image'
 
-export default function Figure(props: ImageProps) {
+export function Figure(props: ImageProps) {
   return (
     <Image
       sizes="100vw"
       className="h-auto w-full"
+      width={600}
+      height={400}
       {...props}
       alt={props.alt || ''}
     />
