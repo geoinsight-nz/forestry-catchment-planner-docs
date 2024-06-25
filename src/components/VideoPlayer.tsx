b647d7dde677b67c6f917b9f77d24d7339879efa
Henry Babbage <hbabbage@gmail.com>
2024-06-25 04:28:55 +0200

    remove problem route api

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,3 @@
-import Video from 'next-video';
-import awesomeVideo from '/videos/get-started.mp4'
-
 export default function VideoPlayer() {
-  return <Video src={awesomeVideo} />;
+  return <></>
 }
