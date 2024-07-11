1f4555b06ecfc11d9583a498025120c9b11fcdfd
Henry Babbage <hbabbage@gmail.com>
2024-07-12 01:53:40 +0200

    Chore: reorganise all images into _images folders

--- src/components/Guides.tsx
+++ src/components/Guides.tsx
@@ -38,7 +38,7 @@ const guides = [
 export function Guides() {
   return (
     <div className="my-16 xl:max-w-none">
-      <Heading level={2} id="guides">
+      <Heading level={2} id="quick-links">
         Quick links
       </Heading>
       <div className="not-prose mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/5 pt-10 sm:grid-cols-2 xl:grid-cols-4 dark:border-white/5">
